244a541e8487b7fba487c4eba43a726c89a37501
Tirso Peguero <tito.peguero@gmail.com>
2014-07-14 09:54:14 -0700

    check in tests for #primes_through and #n_primes

--- spec/prime_spec.rb
+++ spec/prime_spec.rb
@@ -20,9 +20,31 @@ describe "#is_prime?" do
 end
 
 describe "#primes_through" do
+  two = [1, 2]
+  ten = two + [3, 5, 7]
+  twenty_two = ten + [11, 13, 17, 19]
   
+  it "should return all primes <= n" do
+    expect(primes_through(2)).to eq(two)
+    expect(primes_through(10)).to eq(ten)
+    expect(primes_through(22)).to eq(twenty_two)
+  end
 end
 
 describe "#n_primes" do
+  two = [1, 2]
+  five = two + [3, 5, 7]
+  nine = five + [11, 13, 17, 19]
   
+  it "returns the correct number of primes" do
+    expect(n_primes(2).length).to eq(2)
+    expect(n_primes(5).length).to eq(5)
+    expect(n_primes(9).length).to eq(9)
+  end
+  
+  it "returns all the correct primes" do
+    expect(n_primes(2)).to eq(two)
+    expect(n_primes(5)).to eq(five)
+    expect(n_primes(9)).to eq(nine)
+  end
 end
