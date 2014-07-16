60d6a5adb4e51b4af4fb1661b7fc2d9c37685608
Tirso Peguero <tito.peguero@gmail.com>
2014-07-16 09:03:38 -0700

    check in fix for #print_matrix

--- lib/print_matrix.rb
+++ lib/print_matrix.rb
@@ -6,7 +6,6 @@ def print_matrix(matrix)
   pad = matrix.last.last.to_s.length
   
   matrix.map do |row|
-    row.map { |el| add_padding(el.to_s, pad) }
-    puts row.join("  ")
+    puts row.map { |el| add_padding(el.to_s, pad) }.join("  ")
   end
 end
